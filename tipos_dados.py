b782e2dd19f794c2e2f46230154ac82daaef2df5
Marianagarcez-maker <santosm01965@gmail.com>
2025-12-04 16:50:45 -0300

    Add files via upload

---
+++ tipos_dados.py
@@ -0,0 +1,101 @@
+# objetivo mostrar o que esta dentro do parenteses dela no
+# no ambiente de teste 
+# 4  tipos strings float inteiro boleanos
+
+# quebra de linha em python 
+print('isso é\num texto ')
+print()
+print('''
+hdsjka
+klda
+dhasjkld
+dasjkd
+''')
+
+print(10)
+print(10.5)
+print(True, False)
+
+print('Olá seja bem vindo Júlia, \nobrigada volte sempre')
+
+
+3#Função\comando
+
+print('house\'s') 
+print("house's")
+
+# aritmético -  sempre um número como saída *** 
+
+print(10 + 2)
+print(10 - 2)
+print(10 / 2)
+print(10 ** 2)
+print(10 * 2)
+print(10.3 // 2)
+print(10 % 2)
+print(16 ** 0.5)
+
+# lógicos  -  True ou False
+print(10 > 2) # maior
+print(10 < 2) # menor
+print(10 >= 2) # maior ou igual
+print(10 <= 2) # menor ou igual
+print(10 == 2) # comparação
+print(10 != 2) # diferente
+
+vaIiavel = 5
+variavel = 10
+print()
+#NÃO PODE NUMEROS no inicio, NÃO PODE ESPAÇAMENTOS
+# estrutura de dado  palavra  =
+# fracamente tipadas
+# dinamicamente
+# únicas
+# CRIE
+# atribuição
+# 1  dado
+
+numero_1 = 10
+numero_2 = 20
+soma  =  numero_1 + numero_2
+print(soma)
+
+altura  = 1.70
+peso =  70
+imc  =  peso/(altura ** 2)
+print('IMC:', imc)
+
+
+nome     = 'José'
+idade    =  35
+endereco =  'Rua 10, jd x' 
+email =  'jose@gmail.com'
+print('Nome:', nome)
+print('Idade:',idade)
+print('Endereço:', endereco)
+print('E-mail:',email)
+
+print(f'''
+Nome: {nome}
+Idade: {idade}
+Endereço:{endereco}
+E-mail:{email}
+''')
+
+
+
+jhon = 3 
+mary = 5
+Adam = 6 
+
+print('Jhon:', jhon)
+print('Mary', mary)
+print('Adam', Adam)
+
+total_apples = jhon + mary + Adam
+
+print('total_apples:' , total_apples )
+
+print()
+
+
