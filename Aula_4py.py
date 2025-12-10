5dcad01d0e07d9a6726fdc59aa60731f7fff6a9e
Marianagarcez-maker <santosm01965@gmail.com>
2025-12-10 15:02:41 -0300

    Add files via upload

---
+++ Aula_4py.py
@@ -0,0 +1,13 @@
+print( 'DIGITE UM NÚMERO INTEIRO\n')
+
+numero = int(input('Digite AQUI: '))
+ 
+
+if numero > 0:
+    print('numero positivo')
+
+elif numero < 0 :
+    
+    print('o número é negativo')
+else:
+    print('0')
